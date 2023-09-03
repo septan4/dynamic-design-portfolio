8580c1d132978593c02d5511b57bae5f23b64559
septan4 <sepehr75t@gmail.com>
2023-09-03 02:15:40 +0200

    Homepage positions fixed Navbar and footer font color also fixed

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,20 @@
-import { Box, Link} from '@chakra-ui/react'
-import NextLink from "next/link";
+import { Box, Link } from '@chakra-ui/react'
+import NextLink from 'next/link'
 
 const Footer = () => {
   return (
-    <Box align="center" opacity={0.4} fontSize="sm">
+    <Box align="center" opacity={0.4} fontSize="sm" color="#2D2D2D">
       &copy; {new Date().getFullYear()} Zara Yousef. All Rights Reserved.
-        <Link
-            as={NextLink}
-            href="https://www.sepehrtanhaei.com/"
-            passHref
-            scroll={false}
-            target="_blank"
-        >
-            Developed by Sepehr{' '}
-        </Link>
+      <Link
+        as={NextLink}
+        href="https://www.sepehrtanhaei.com/"
+        passHref
+        scroll={false}
+        target="_blank"
+        ml="1"
+      >
+        Developed by Sepehr{' '}
+      </Link>
     </Box>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -26,35 +26,45 @@ const Home = () => (
     <Container maxW="container.xl" h="100vh">
       <Center h="100%">
         <Section delay={0.1}>
-          <Box display="flex" alignItems="end">
-            <Image
-              src={HI}
-              width={116}
-              height={116}
-              alt="Picture of the author"
-            />
-            <Heading as="h2" variant="page-title" ml={-10}>
-              I&apos;m Zara
-            </Heading>
+          <Box display="flex" alignItems="center" flexDirection="column">
+            <Box display={'flex'} flexDirection={'column'} alignItems={'start'}>
+              <Box display="flex" alignItems="end">
+                <Image
+                  src={HI}
+                  width={116}
+                  height={116}
+                  alt="Picture of the author"
+                />
+                <Heading as="h2" variant="page-title" ml={-10}>
+                  I&apos;m Zara
+                </Heading>
+              </Box>
+              <Text
+                color="#2D2D2D"
+                fontSize="82px"
+                fontWeight={700}
+                fontFamily="Exo Variable, sans-serif"
+              >
+                Product Designer
+              </Text>
+            </Box>
+            <Text
+              align="center"
+              fontSize="16px"
+              fontWeight={600}
+              fontFamily="Exo Variable, sans-serif"
+              maxW="container.sm"
+              lineHeight="140%"
+              color="#2D2D2D"
+              textTransform="capitalize"
+            >
+              I&apos;m a UX/UI & Product Designer with +3 years experience. B
+              ased in Istanbul.
+              <br /> conceptualizing and crafting digital products, helping
+              businesses and startups <br /> expand their capacity for impact
+              🚀.
+            </Text>
           </Box>
-          <Text
-            color="#2D2D2D"
-            fontSize="82px"
-            fontWeight={700}
-            fontFamily="Exo Variable, sans-serif"
-          >
-            Product Designer
-          </Text>
-          <Text
-            align="center"
-            fontSize="16px"
-            fontWeight={600}
-            fontFamily="Exo Variable, sans-serif"
-          >
-            I&apos;m a UX/UI & Product Designer with +3 years experience. Based
-            in Istanbul. Conceptualizing and crafting digital products, helping
-            businesses and startups expand their capacity for impact 🚀.
-          </Text>
         </Section>
       </Center>
     </Container>
